c17f024b0429d9a3c4f2526d2ff3fffd467188c0
Eshan M. Herath <eshan.m.herath@gmail.com>
2020-12-15 14:54:06 +0530

    Feat: Variable Sizes

---
+++ L1/L103_sizes_of_datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "bool size = " <<  sizeof(bool) << std::endl;
+    
+    std::cout << "char size = " <<  sizeof(char) << std::endl;   
+    
+    std::cout << "short size = " <<  sizeof(short) << std::endl;
+    std::cout << "int size = " <<  sizeof(int) << std::endl;
+    std::cout << "long size = " <<  sizeof(long) << std::endl;
+    
+    std::cout << "float size = " <<  sizeof(float) << std::endl;
+    std::cout << "double size = " <<  sizeof(double) << std::endl;
+    
+    
+    return 0;
+}
